7825ae918482ac8acd1cfe48805270ae2896ff6e
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-10-05 17:10:31 +0700

    style global

--- src/utils/global_style.tsx
+++ src/utils/global_style.tsx
@@ -1,14 +1,45 @@
 import { StyleSheet } from 'react-native';
 
 const stylesGlobal = StyleSheet.create({
-    view20: {
+    enter10: {
+        height: 10,
+    },
+    enter20: {
         height: 20,
     },
-    view30: {
+    enter30: {
         height: 30,
     },
-    view40: {
+    enter40: {
         height: 40,
+    },
+    header1: {
+        fontSize: 24,
+        fontFamily: 'Ubuntu-Bold'
+    },
+    header2: {
+        fontSize: 20,
+        fontFamily: 'Ubuntu-Bold'
+    },
+    header3: {
+        fontSize: 14,
+        fontFamily: 'Ubuntu-Bold'
+    },
+    subtitle: {
+        fontSize: 14,
+        fontFamily: 'Ubuntu-Light'
+    },
+    colorWhite: {
+        color: '#FFFFFF',
+    },
+    colorPremier: {
+        color: '#2F5664',
+    },
+    backroundPremier: {
+        backgroundColor: '#6399B0'
+    },
+    backroundWhite: {
+        backgroundColor: '#FFFFFF'
     },
 
 })
