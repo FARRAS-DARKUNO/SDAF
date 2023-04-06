b7ac6999e8394bd23af7b1d7af0690f5fe5ed8d7
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2023-04-06 16:06:06 +0700

    Lupa Ngecommit

--- src/utils/indicator.ts
+++ src/utils/indicator.ts
@@ -1,11 +1,11 @@
 export const indicator = ({angin,gelombang} : Props) => {
-    if (angin <= 21 || gelombang <= 2.9) {
+    if (angin <= 10.8 || gelombang <= 4) {
         return "Cuaca Aman"
     }
-    else if (angin <=27 || gelombang <= 3.9){
+    else if (angin <=17.2 || gelombang <= 5.5){
         return "Berhati-Hati"
     }
-    else if (angin > 27 || gelombang > 3.9) {
+    else if (angin > 17.2 || gelombang > 5.5) {
         return "Cuaca Tidak Aman"
     }
 
